99192070d51bc8d06ae7c43539f5ff1528726ce7
koblmom <nayun9191@gamil.com>
2024-03-07 05:13:02 +0900

    feat: loginform 유효성 검사

--- js/signin.js
+++ js/signin.js
@@ -36,10 +36,35 @@ function validatePassword() {
   }
 }
 
-const emailInput = document.querySelector("#emailInput");
+const emailInput = document.querySelector(".emailInput");
 const emailErrorMessage = document.querySelector(".emailErrorMessage");
-const pwInput = document.querySelector("#pwInput");
+const pwInput = document.querySelector(".pwInput");
 const pwErrorMessage = document.querySelector(".pwErrorMessage");
 
 emailInput.addEventListener("focusout", validateEmail);
 pwInput.addEventListener("focusout", validatePassword);
+
+/* Submit loginform */
+
+document
+  .querySelector(".submit-button")
+  .addEventListener("click", submitLoginForm);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); // Enter 키의 기본 동작(폼 제출) 막기
+    submitLoginForm();
+  }
+});
+
+function submitLoginForm() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email === "test@codeit.com" && password === "codeit101") {
+    location.href = "./folder.html";
+  } else {
+    appendEmailError("이메일을 확인 해주세요.");
+    appendPWError("비밀번호를 확인해 주세요.");
+  }
+}
